2622d6844ac37633bb3b292ccc05a9d18a162aa5
JungWwon <kimjw7020@gmail.com>
2022-02-11 18:51:29 +0900

    make json of users

--- server.js
+++ server.js
@@ -2,8 +2,19 @@ const express = require('express'); //express can be used
 
 const server = express(); //making server
 
+const users = [
+    {
+        name: "a",
+        amount: 1
+    },
+    {
+        name: "b",
+        amount: 5
+    }
+] //making json of users
+
 server.listen(3000, () => {
     console.log("running");
 }); //opening the server
 
-//next -> to execute the server, write ' node server' In terminal 
+//next -> to execute the server, write ' node server' In terminal.
